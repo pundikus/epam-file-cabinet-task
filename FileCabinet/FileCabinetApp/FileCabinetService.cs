b389a675d1fffd892cd3eb837678287e7310a1e3
pundikus <nik.pundis@mail.ru>
2019-12-15 15:48:46 +0300

    Add guard clauses to CreateRecord method.

--- FileCabinet/FileCabinetApp/FileCabinetService.cs
+++ FileCabinet/FileCabinetApp/FileCabinetService.cs
@@ -10,6 +10,44 @@ namespace FileCabinetApp
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short cabinetNumber, decimal salary, char category)
         {
+            if (firstName == null || string.IsNullOrEmpty(firstName.Trim()))
+            {
+                throw new ArgumentNullException($"{nameof(firstName)} cannot be empty.");
+            }
+
+            if (firstName.Length < 2 || firstName.Length > 60)
+            {
+                throw new ArgumentException($"{nameof(firstName)} not correct.");
+            }
+
+            if (lastName == null || string.IsNullOrEmpty(lastName.Trim()))
+            {
+                throw new ArgumentNullException($"{nameof(lastName)} cannot be empty.");
+            }
+
+            if (lastName.Length < 2 || lastName.Length > 60)
+            {
+                throw new ArgumentException($"{nameof(lastName)} not correct.");
+            }
+
+            DateTime minDate = new DateTime(1950, 1, 1);
+            DateTime maxDate = DateTime.Now;
+
+            if (dateOfBirth < minDate || dateOfBirth > maxDate)
+            {
+                throw new ArgumentException($"{nameof(dateOfBirth)} not correct.");
+            }
+
+            if (cabinetNumber < 1 || cabinetNumber > 1500)
+            {
+                throw new ArgumentException($"{nameof(cabinetNumber)} not correct.");
+            }
+
+            if (salary < 0 || salary > decimal.MaxValue)
+            {
+                throw new ArgumentException($"{nameof(salary)} not correct.");
+            }
+
             var record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
