755aec33c64ed5356acd3bbae3ecab40596627d9
pundikus <nik.pundis@mail.ru>
2020-01-24 21:14:26 +0300

    Add implementation for template 'Chain of Responsibility'.

---
+++ FileCabinet/FileCabinetApp/CommandHandlerBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// This class initialize Handler.
+    /// </summary>
+    public class CommandHandlerBase : ICommandHandler
+    {
+        private ICommandHandler nextHandler;
+
+        /// <summary>
+        /// Handler the request.
+        /// </summary>
+        /// <param name="request">Request to handle.</param>
+        /// <returns>The object back or null.</returns>
+        public virtual object Handle(AppCommandRequest request)
+        {
+            if (this.nextHandler != null)
+            {
+                return this.nextHandler.Handle(request);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Set the next handler.
+        /// </summary>
+        /// <param name="commandHandler">Handler to set.</param>
+        /// <returns>Handler back.</returns>
+        public ICommandHandler SetNext(ICommandHandler commandHandler)
+        {
+            this.nextHandler = commandHandler;
+
+            return commandHandler;
+        }
+    }
+}
---
+++ FileCabinet/FileCabinetApp/ICommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// This interface sets behavor.
+    /// </summary>
+    public interface ICommandHandler
+    {
+        /// <summary>
+        /// Set the next handler.
+        /// </summary>
+        /// <param name="commandHandler">Handler to set.</param>
+        /// <returns>Handler back.</returns>
+        public ICommandHandler SetNext(ICommandHandler commandHandler);
+
+        /// <summary>
+        /// Handler the request.
+        /// </summary>
+        /// <param name="request">Request to handle.</param>
+        /// <returns>The object back or null.</returns>
+        public object Handle(AppCommandRequest request);
+    }
+}
